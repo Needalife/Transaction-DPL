1bdeae80978ae6acfd3a171b4d73d30177fad2f0
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-03 14:59:09 +0700

    Update Overview.py

--- src/pages/Overview.py
+++ src/pages/Overview.py
@@ -1,4 +1,4 @@
-import plotly.express as px,streamlit as st,pandas as pd, numpy as np # type: ignore
+import altair as alt,streamlit as st,pandas as pd, numpy as np # type: ignore
 import requests,os,sys,pymongo # type: ignore
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -10,23 +10,44 @@ def connectMongo():
     mongoHandler = mongo(readMongoURI(),'data')
     return mongoHandler
 
-@st.cache_data
 def getLatestRecords(records):
     job = connectMongo()   
     df = pd.json_normalize(list(job.getNewestRecords('raw',records)))
     return df.drop('_id', axis=1)
 
+def plotAltairChart(df,number):
+    # Mapping status to numerical values for count purposes
+    status_mapping = {'error': -1, 'ongoing': 0, 'success': 1}
+    df['status_num'] = df['status'].map(status_mapping)
+
+    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.floor('T')
+
+    # Transform data to count each status per timestamp
+    status_count = df.groupby(['timestamp', 'status']).size().reset_index(name='count')
+
+    # Plotting the stacked bar chart
+    st.subheader('Transaction Status')
+    chart = alt.Chart(status_count).mark_bar().encode(
+        x='timestamp:T',
+        y='count:Q',
+        color=alt.Color('status:N', scale=alt.Scale(domain=['error', 'ongoing', 'success'],
+                                                    range=['#FF4B4B', '#FFD700', '#32CD32'])),
+    ).properties(
+        width=800,
+        height=400
+    ).interactive()
+
+    st.altair_chart(chart)
+
+st.button("Reload")
 st.title("Overview")
-number = st.slider("Latest records", 0, 100)
+number = st.slider("Number of records", 0, 700)
 
+df = getLatestRecords(number)
 if st.checkbox('Show raw data'):
-    st.dataframe(getLatestRecords(number))
+    st.dataframe(df)
     
-st.subheader('Transaction Status')
-status_timestamp = getLatestRecords(number)[["status","timestamp"]]
-st.line_chart(status_timestamp,x='timestamp',y='status')
-
-
+plotAltairChart(df,number=number)
 
 
 
