7fde0dd660c967285d8e688df0cefbec90c481f7
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-04 13:52:03 +0700

    Update Terminal.py

--- src/pages/Terminal.py
+++ src/pages/Terminal.py
@@ -1,13 +1,6 @@
 import streamlit as st #type:ignore
 import subprocess,requests #type:ignore
 
-def sessionStateButtons(number_of_buttons:int):
-    i = 0
-    while i < number_of_buttons:
-        i += 1
-        if f"button{i}" not in st.session_state:
-            st.session_state[f"button{i}"] = False
-    
 # Function to execute cloud commands
 def invokeTransactionProducer():
     try:
@@ -20,8 +13,6 @@ def invokeTransactionProducer():
         
 st.title("Terminal")
 
-sessionStateButtons(2)
-
 # Button 1: Generate Data
 if st.button("Generate data"):
     result = invokeTransactionProducer()
@@ -30,28 +21,8 @@ if st.button("Generate data"):
     else:
         st.success(f"Successfully pushed {len(result)} transactions to GCS")
         st.code(result)
-        st.session_state['button1'] = True
-        st.session_state['button1_result'] = result
-
-# Display result if button 1 was pressed
-if st.session_state['button1']:
-    st.code(st.session_state['button1_result'])
 
 # Button 2: Perform some action without reloading the page
-if st.button('Test'):
-    if st.session_state['button1']:
-        st.write('Do something..')
-        st.session_state['button2'] = not st.session_state['button2']
-
-# Optionally, you can add some logic based on the state of button 2
-if st.session_state['button2']:
-    st.write('Button 2 was pressed and toggled its state.')
-
-st.write(
-    f"""
-    ## Session state:
-    {st.session_state["button1"]=}
-
-    {st.session_state["button2"]=}
-    """
-)
+if st.button('Set bucket lifecycle'):
+    st.session_state['button2'] = not st.session_state['button2']
+st.write('After')
