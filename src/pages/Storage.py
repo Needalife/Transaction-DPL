ac83ad217df74cf6c2f6ee5ad8075f39db5c62ec
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-10 14:45:25 +0700

    Update Storage.py

--- src/pages/Storage.py
+++ src/pages/Storage.py
@@ -33,7 +33,6 @@ def invokeGetBucketLifeCycle(bucket):
     except Exception as e:
         st.error(f"{e}")
 
-@st.cache_resource
 def connectMongo():
     response = requests.get("https://us-central1-project-finance-400806.cloudfunctions.net/get-mongo-uri")
     if response.status_code == 200:
