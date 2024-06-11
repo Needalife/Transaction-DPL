9eef3f91842744ab0f9ee610a76746248db55b5f
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-11 11:02:30 +0700

    Update Storage.py

--- src/pages/Storage.py
+++ src/pages/Storage.py
@@ -24,6 +24,18 @@ collection = database['raw']
 rule = database['rule']
 rule = rule.find_one({})
 
-with collection.watch() as stream:
-    for changes in stream:
-        st.write(changes['operationType'])
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.container():
+    st.write("This is one element")
+    st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
