50f9ccca743712e42e771f16be9384d76e9e64a9
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-11 10:56:28 +0700

    Update Storage.py

--- src/pages/Storage.py
+++ src/pages/Storage.py
@@ -1,6 +1,29 @@
 import altair as alt,streamlit as st,pandas as pd, numpy as np # type: ignore
 import requests,time # type: ignore
 from utils.mongo import *
+from pymongo import MongoClient # type: ignore
+
+@st.cache_resource
+def connectMongo():
+    response = requests.get("https://us-central1-project-finance-400806.cloudfunctions.net/get-mongo-uri")
+    if response.status_code == 200:
+        response.close()
+    
+    mongoHandler = mongo(response.text,'data')
+    
+    return mongoHandler
 
 #UI start
-st.title("Storage")
+
+uri = "mongodb+srv://gauakanguyen:AOMhWKFdmlSO6kcU@transactiondata.wecxmij.mongodb.net/?retryWrites=true&w=majority&appName=transactionData"
+client = MongoClient(uri)
+
+database = client['data']
+collection = database['raw']
+
+rule = database['rule']
+rule = rule.find_one({})
+
+with collection.watch() as stream:
+    for changes in stream:
+        st.write(changes)
