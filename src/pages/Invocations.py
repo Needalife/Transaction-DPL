18294ac9589890150a2db99b2672f7b08471bfd1
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-12 14:02:51 +0700

    Update Invocations.py

--- src/pages/Invocations.py
+++ src/pages/Invocations.py
@@ -1,14 +1,24 @@
 import pandas as pd, json,requests,streamlit as st #type:ignore
 
-input_data = {'rows':100}
+
 url = 'https://us-central1-project-finance-400806.cloudfunctions.net/getFunctionLogs'
-while True:
+
+def getNewData(rows):
+    input_data = {'rows':rows}
     result = requests.post(url,json=input_data)
 
     if result.status_code == 200:
         data = json.loads(result.text)
         data = pd.DataFrame(data)
-        st.dataframe(pd.DataFrame(data))
+        return pd.DataFrame(data)
     else:
         print(result.status_code)
         st.error(f'Error occurred: {result.status_code}', icon="🚨")
+
+st.title("Function Status")
+placeholder = st.empty()
+while True:
+    df = getNewData(100)
+    
+    with placeholder.container():
+        st.dataframe(df)
