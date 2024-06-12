caf3a1b7e9ec4418c3efb45e351ca954ad94c391
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-12 13:50:15 +0700

    Update Invocations.py

---
+++ src/pages/Invocations.py
@@ -0,0 +1,13 @@
+import pandas as pd, json,requests,streamlit as st #type:ignore
+
+input_data = {'rows':5}
+url = 'https://us-central1-project-finance-400806.cloudfunctions.net/getFunctionLogs'
+result = requests.post(url,json=input_data)
+
+if result.status_code == 200:
+    data = json.loads(result.text)
+    data = pd.DataFrame(data)
+    st.dataframe(pd.DataFrame(data))
+
+else:
+    print(result.status_code)
