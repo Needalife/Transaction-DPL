df210554ccf37704eae3fb317ab3e13514745727
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-12 15:07:50 +0700

    Update Invocations.py

--- src/pages/Invocations.py
+++ src/pages/Invocations.py
@@ -35,12 +35,12 @@ while True:
     df_pivot = df_counts.pivot(index='time', columns='function', values='count').fillna(0)
 
     with placeholder.container():
-        st.write(df_pivot)
+        
         # Display first and last time and their difference
         if not df.empty:
             st.write(f"Time difference: {df['time'].iloc[-1] - df['time'].iloc[0]}")
             st.dataframe(df)
-
+        st.write(df_pivot)
         # Plotting the line chart
         st.line_chart(df_pivot)
         
