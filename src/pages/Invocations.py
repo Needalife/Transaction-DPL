ee3e20a89a392497d331cfb5101747b7671c557f
Vally <113294564+Needalife@users.noreply.github.com>
2024-06-24 15:41:20 +0700

    Update Invocations.py

--- src/pages/Invocations.py
+++ src/pages/Invocations.py
@@ -46,7 +46,7 @@ while True:
             #Plot altair chart
             st.write('**Latency**')
             # Create the Altair chart
-            chart = alt.Chart(df).mark_area().encode(
+            chart = alt.Chart(df).mark_point().encode(
                 x='time:T',
                 y='latency:Q',
                 color='function:N'
